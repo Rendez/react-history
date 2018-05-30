11f686f0cc74324e66a12bddf377d02df7cbe0c3
Luis Merino <luis.merino@researchgate.net>
2018-05-30 16:49:29 +0200

    Fix eslint erroring files with semi colons

--- modules/HashHistory.js
+++ modules/HashHistory.js
@@ -1,8 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
-import createHashHistory from "history/createHashHistory";
-import { canUseDOM } from "history/DOMUtils";
-import { history as historyType } from "./PropTypes";
+import React from "react"
+import PropTypes from "prop-types"
+import createHashHistory from "history/createHashHistory"
+import { canUseDOM } from "history/DOMUtils"
+import { history as historyType } from "./PropTypes"
 
 /**
  * Manages session history using window.location.hash.
@@ -13,46 +13,46 @@ class HashHistory extends React.Component {
     getUserConfirmation: PropTypes.func,
     hashType: PropTypes.oneOf(["hashbang", "noslash", "slash"]),
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired
-  };
+  }
 
   static childContextTypes = {
     history: historyType.isRequired
-  };
+  }
 
   getChildContext() {
-    return { history: this.history };
+    return { history: this.history }
   }
 
   constructor(props) {
-    super(props);
-    
-    const { basename, getUserConfirmation, hashType } = this.props;
+    super(props)
+
+    const { basename, getUserConfirmation, hashType } = this.props
 
     if (canUseDOM) {
       this.history = createHashHistory({
         basename,
         getUserConfirmation,
         hashType
-      });
+      })
 
       // Do this here so we catch actions in cDM.
-      this.unlisten = this.history.listen(() => this.forceUpdate());
+      this.unlisten = this.history.listen(() => this.forceUpdate())
     } else {
-      this.history = {};
+      this.history = {}
     }
   }
 
   componentWillUnmount() {
-    this.unlisten();
+    this.unlisten()
   }
 
   render() {
-    const { children } = this.props;
+    const { children } = this.props
 
     return typeof children === "function"
       ? children(this.history)
-      : React.Children.only(children);
+      : React.Children.only(children)
   }
 }
 
-export default HashHistory;
+export default HashHistory
--- modules/Prompt.js
+++ modules/Prompt.js
@@ -1,53 +1,53 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { polyfill } from 'react-lifecycles-compat';
-import { canUseDOM } from "history/DOMUtils";
-import { history as historyType } from "./PropTypes";
+import React from "react"
+import PropTypes from "prop-types"
+import { polyfill } from "react-lifecycles-compat"
+import { canUseDOM } from "history/DOMUtils"
+import { history as historyType } from "./PropTypes"
 
 class Prompt extends React.Component {
   static propTypes = {
     when: PropTypes.bool,
     message: PropTypes.oneOfType([PropTypes.func, PropTypes.string]).isRequired
-  };
+  }
 
   static contextTypes = {
     history: historyType.isRequired
-  };
+  }
 
   static defaultProps = {
     when: true
-  };
+  }
 
   constructor(props, context) {
-    super(props, context);
+    super(props, context)
 
     if (canUseDOM) {
       if (this.props.when) {
-        this.enable(this.props.message);
+        this.enable(this.props.message)
       }
     }
   }
 
   enable(message) {
-    if (this.unblock) this.unblock();
-    this.unblock = this.context.history.block(message);
+    if (this.unblock) this.unblock()
+    this.unblock = this.context.history.block(message)
   }
 
   disable() {
     if (this.unblock) {
-      this.unblock();
-      this.unblock = null;
+      this.unblock()
+      this.unblock = null
     }
   }
 
   getSnapshotBeforeUpdate(prevProps) {
     if (this.props.when) {
       if (!prevProps.when || prevProps.message !== this.props.message)
-        this.enable(this.props.message);
+        this.enable(this.props.message)
     } else {
-      this.disable();
+      this.disable()
     }
-    return null;
+    return null
   }
 
   componentDidUpdate() {
@@ -55,15 +55,15 @@ class Prompt extends React.Component {
   }
 
   componentWillUnmount() {
-    this.disable();
+    this.disable()
   }
 
   render() {
-    return null;
+    return null
   }
 }
 
 // Polyfill your component so the new lifecycles will work with older versions of React:
-polyfill(Prompt);
+polyfill(Prompt)
 
-export default Prompt;
+export default Prompt
